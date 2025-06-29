4de044603b3775eba71ffc380de341a5a40098c7
Kundan-ionicDev <kundan.mobileappdev@gmail.com>
2025-06-29 21:44:18 +0530

    production version

--- dist/mybusiness/server/angular-app-engine-manifest.mjs
+++ dist/mybusiness/server/angular-app-engine-manifest.mjs
@@ -1,6 +1,6 @@
 
 export default {
-  basePath: '/infinitysol',
+  basePath: '/',
   supportedLocales: {
   "en-US": ""
 },
--- dist/mybusiness/server/angular-app-manifest.mjs
+++ dist/mybusiness/server/angular-app-manifest.mjs
@@ -2,13 +2,13 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: '/infinitysol/',
+  baseHref: '/',
   locale: undefined,
   routes: undefined,
   entryPointToBrowserMapping: {},
   assets: {
-    'index.csr.html': {size: 8283, hash: 'd74cd771125d138de44ca5b10c8ca23413ea97bf3bed932033aa9c18c5157f97', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 5332, hash: '517ab8bb8f2b7dbbd02d684a5002572c88512d9e09e2c4a133c6e578d78f9e72', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'styles-UGL6THQB.css': {size: 264971, hash: 'rvy8sqM8+jk', text: () => import('./assets-chunks/styles-UGL6THQB_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 8271, hash: '5d14e9ab1f7ba4771b529c80f38ef60f1280fe3f84ab83f26fe128bf26d4b6f2', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 5320, hash: 'b1c7aa08ed9d1d31af771919008da8f7a503daf71f3cc7aef7d181090fe748fb', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'styles-5MYPSVJR.css': {size: 265691, hash: 'Sfkfpt0jVo8', text: () => import('./assets-chunks/styles-5MYPSVJR_css.mjs').then(m => m.default)}
   },
 };
